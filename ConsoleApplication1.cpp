1bc11cae9277103f42b69c32983c751223bf1711
Rojuz <andriuskeviciusrojus@gmail.com>
2024-08-22 15:46:54 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <locale>
+#include <cmath>
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+    bool a = false;
+    cout << a;
+    if(hex   )
+    return 0;
+}
+
